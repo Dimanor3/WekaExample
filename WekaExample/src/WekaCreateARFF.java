b34600d87a276169c2eda93079dbe4b84587b5b9
Bijan Razavi <brazavi@uncc.edu>
2017-02-02 23:20:33 -0500

    Adds data into ARFF file, creates and saves ARFF file.

--- WekaExample/src/WekaCreateARFF.java
+++ WekaExample/src/WekaCreateARFF.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
+import weka.core.Instance;
+
 import java.util.*;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
@@ -14,12 +16,18 @@ public class WekaCreateARFF {
 		// Holds an ArrayList of attributes.
 		ArrayList<Attribute> attributes = new ArrayList<Attribute> ();
 		
-		// Holds all of the nominal values for nominal attributes
+		// Holds all of the nominal values for nominal attributes.
 		ArrayList<String> politicalPartyVals = new ArrayList<String> ();
 		ArrayList<String> stateVals = new ArrayList<String> ();
 		
 		// Holds data from file.
 		ArrayList<String> data = new ArrayList<String> ();
+		
+		// Holds split data from file.
+		String[] dataSplit;
+		
+		// Data dump
+		double[] vals;
 		
 		// Temporarily holds each read line from the selected file.
 		String temp = null;
@@ -37,7 +45,6 @@ public class WekaCreateARFF {
 			
 			// Reads the selected file.
 			while ((temp = bufferedReader.readLine()) != null) {
-				System.out.println (temp);
 				data.add (temp);
 			}
 			
@@ -49,7 +56,7 @@ public class WekaCreateARFF {
 			System.out.println ("Error reading file!");
 		}
 		
-		// Adds all the values for each nominal type attribute
+		// Adds all the values for each nominal type attribute.
 		politicalPartyVals.add ("democrat");
 		politicalPartyVals.add ("republican");
 		stateVals.add ("CA");
@@ -65,12 +72,32 @@ public class WekaCreateARFF {
 		attributes.add (new Attribute ("state", stateVals));
 		attributes.add (new Attribute ("birth date", "yyyy-MM-dd"));
 		
-		// Create Instances object
+		// Create Instances object.
 		dataSet = new Instances ("SimpleARFF", attributes, 0);
 		
-		// Add the data
+		
+		// Add the data to the ARFF file.
 		for (String line : data) {
+			vals = new double[dataSet.numAttributes ()];
 			
+			dataSplit = line.split(",");
+			
+			vals[0] = Double.parseDouble (dataSplit[0]);
+			vals[1] = dataSet.attribute (1).addStringValue (dataSplit[1]);
+			vals[2] = politicalPartyVals.indexOf (dataSplit[2].replaceAll("[ ']", ""));
+			vals[3] = stateVals.indexOf (dataSplit[3].replaceAll("[ ']", ""));
+			vals[4] = dataSet.attribute (4).parseDate (dataSplit[4]);
+			
+			dataSet.add (new DenseInstance (1.0, vals));
 		}
+		
+		// Print the ARFF file out.
+		System.out.println (dataSet);
+		
+		// Create ARFF file.
+	    ArffSaver arffSaverInstance = new ArffSaver();
+	    arffSaverInstance.setInstances (dataSet);
+	    arffSaverInstance.setFile (new File ("SimpleARFF.arff"));
+	    arffSaverInstance.writeBatch ();
 	}
 }
