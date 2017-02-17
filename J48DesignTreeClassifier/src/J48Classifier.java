df075372fe08359bda8e8bf2261fa36fca67066f
Bijan Razavi <brazavi@uncc.edu>
2017-02-17 01:41:57 -0500

    Update

--- J48DesignTreeClassifier/src/J48Classifier.java
+++ J48DesignTreeClassifier/src/J48Classifier.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
-import weka.core.converters.ArffLoader.ArffReader;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class J48Classifier {
@@ -18,7 +20,35 @@ public class J48Classifier {
 
 		// Reads ARFF file.
 		DataSource arffFile = new DataSource ("weather.nominal.arff");
+
+		// Temporarily holds each read line from the selected file.
+		String temp = null;
+
+		ArrayList<String> testData = new ArrayList<String> ();
 		
+		Instances testResult;
+
+		// Tries to read the file and if it can't it lets the user know.
+		try {
+			// Reads text from the selected file.
+			FileReader fileReader = new FileReader ("testData.txt");
+
+			// Opens the selected file to read it.
+			BufferedReader bufferedReader = new BufferedReader (fileReader);
+
+			// Reads the selected file.
+			while ( (temp = bufferedReader.readLine ()) != null) {
+				testData.add (temp);
+			}
+
+			// Closes the selected file after it's done reading.
+			bufferedReader.close ();
+		} catch (FileNotFoundException ex) {
+			System.out.println ("Unable to open the file!");
+		} catch (IOException ex) {
+			System.out.println ("Error reading file!");
+		}
+
 		// Converts and stores ARFF file.
 		Instances arffData = arffFile.getDataSet ();
 
@@ -44,4 +74,3 @@ public class J48Classifier {
 		System.out.println (eval.toMatrixString ());
 	}
 }
-
