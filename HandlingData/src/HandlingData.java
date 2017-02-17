346234261cce92e562ad5756348d8761bde17a37
dimanor3 <bijanrazavi15@yahoo.com>
2017-02-17 10:39:51 -0500

    Now checks for either .CSV or .ARFF file to load in, setting a filter to replace missing values in the data (WIP).

--- HandlingData/src/HandlingData.java
+++ HandlingData/src/HandlingData.java
@@ -6,17 +6,39 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.unsupervised.attribute.ReplaceMissingValues;
 
 public class HandlingData {
 	public static void main (String[] args) throws Exception {
-		// Creates a CSVLoader to load the data from cars-missing.csv
+		// Creates a CSVLoader to load the data from cars-missing.csv.
 		CSVLoader csvLoader = new CSVLoader ();
 		
-		// Sets the data source for the CSVLoader
-		csvLoader.setSource (new File ("cars-missing.csv"));
+		m_ReplaceMissingValues = new ReplaceMissingValues ();
 		
-		// Stores the data found from cars-missing.csv in an Instances variable
-		Instances carsMissing = csvLoader.getDataSet ();
+		// Reads ARFF file.
+		DataSource arffFile;
+
+		// Creates an Instances variable to hold cars-missing's data.
+		Instances carsMissing = null;
+		
+		// Checks to see whether or not the file exists in .CSV format,
+		// if it doesn't it then checks for to see if the file exists in
+		// .ARFF format, if it doesn't it then lets the user know that the
+		// file doesn't exist.
+		if (new File ("cars-missing.csv").exists ()) {
+			// Sets the data source for the CSVLoader
+			csvLoader.setSource (new File ("cars-missing.csv"));
+
+			// Stores the data found from cars-missing.csv in an Instances variable.
+			carsMissing = csvLoader.getDataSet ();
+		} else if (new File ("cars-missing.arff").exists ()) {
+			arffFile = new DataSource ("cars-missing.arff");
+			
+			carsMissing = arffFile.getDataSet ();
+		} else {
+			System.out.println ("This file doesn't exist!");
+		}
 		
 		// Makes the last attribute the class.
 		carsMissing.setClassIndex (carsMissing.numAttributes () - 1);
